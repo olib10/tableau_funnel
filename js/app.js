e0755d1afa27637e45919b59def1814a3d37663c
Oliver Bruski <oliver@bru.ski>
2019-11-21 15:56:19 +0100

    debug2

--- js/app.js
+++ js/app.js
@@ -1,72 +1,70 @@
 'use strict';
 
 (function () {
-  $(document).ready(function () {
-    tableau.extensions.initializeAsync({ 'configure':configure }).then(function () {
-      drawChartJS();
-      unregisterSettingsEventListener = tableau.extensions.settings.addEventListener(tableau.TableauEventType.SettingsChanged, (settingsEvent) => {
-        drawChartJS();
-      });
-    }, function () { console.log('Error while Initializing: ' + err.toString()); });
-  });
-
-  function drawChartJS() {
-
-    var worksheetName = tableau.extensions.settings.get("worksheet");
-    var categoryColumnNumber = tableau.extensions.settings.get("categoryColumnNumber");
-    var valueColumnNumber = tableau.extensions.settings.get("valueColumnNumber");
-
-    const worksheets=tableau.extensions.dashboardContent.dashboard.worksheets;
-    var worksheet=worksheets.find(function (sheet) {
-      return sheet.name===worksheetName;
+    $(document).ready(function () {
+        tableau.extensions.initializeAsync({ 'configure': configure }).then(function () {
+            drawChartJS();
+            let unregisterSettingsEventListener = tableau.extensions.settings.addEventListener(tableau.TableauEventType.SettingsChanged, (settingsEvent) => {
+                drawChartJS();
+            });
+        }, function () { console.log('Error while Initializing: ' + err.toString()); });
     });
-    worksheet.getSummaryDataAsync().then(function (sumdata) {
-    console.log(sumdata);
-    //   var labels = [];
-      var data = [];
-      var worksheetData = sumdata.data;
-      
-      for (var i=0; i<worksheetData.length; i++) {
-        data.push(worksheetData[i][categoryColumnNumber-1].formattedValue, worksheetData[i][valueColumnNumber-1].value);
-      }
-      console.log(data);
 
-    //   let ctx = new D3Funnel("#myChart");
-    //   let options = {
-    //       chart: {
-    //           animate: 100
-    //       },
-    //       block: {
-    //           dynamicHeight: true
-    //       }
-    //   }
-    //   ctx.draw(data, options);
-    //   var myChart = new Chart(ctx, {
-    //     type: 'doughnut',
-    //     data: {
-    //       labels: labels,
-    //       datasets: [{
-    //          backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-    //          data: data
-    //       }]
-    //     }
-    //   });
-    });
-  }
+    function drawChartJS() {
 
-  function configure() {
-    const popupUrl=`${window.location.href}/dialog.html`;
-    let defaultPayload="";
-    tableau.extensions.ui.displayDialogAsync(popupUrl, defaultPayload, { height:300, width:500 }).then((closePayload) => {
-      drawChartJS();
-    }).catch((error) => {
-      switch (error.errorCode) {
-        case tableau.ErrorCodes.DialogClosedByUser:
-          console.log("Dialog was closed by user");
-          break;
-        default:
-          console.error(error.message);
-      }
-    });
-  }
+        var worksheetName = tableau.extensions.settings.get("worksheet");
+        var categoryColumnNumber = tableau.extensions.settings.get("categoryColumnNumber");
+        var valueColumnNumber = tableau.extensions.settings.get("valueColumnNumber");
+
+        const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+        var worksheet = worksheets.find(function (sheet) {
+            return sheet.name === worksheetName;
+        });
+        worksheet.getSummaryDataAsync().then(function (sumdata) {
+            var data = [];
+            var worksheetData = sumdata.data;
+
+            worksheetData.forEach(function (w) {
+                data.push([w[categoryColumnNumber - 1].formattedValue, w[valueColumnNumber - 1].value]);
+            });
+            console.log(data);
+
+            //   let ctx = new D3Funnel("#myChart");
+            //   let options = {
+            //       chart: {
+            //           animate: 100
+            //       },
+            //       block: {
+            //           dynamicHeight: true
+            //       }
+            //   }
+            //   ctx.draw(data, options);
+            //   var myChart = new Chart(ctx, {
+            //     type: 'doughnut',
+            //     data: {
+            //       labels: labels,
+            //       datasets: [{
+            //          backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+            //          data: data
+            //       }]
+            //     }
+            //   });
+        });
+    }
+
+    function configure() {
+        const popupUrl = `${window.location.href}/dialog.html`;
+        let defaultPayload = "";
+        tableau.extensions.ui.displayDialogAsync(popupUrl, defaultPayload, { height: 300, width: 500 }).then((closePayload) => {
+            drawChartJS();
+        }).catch((error) => {
+            switch (error.errorCode) {
+                case tableau.ErrorCodes.DialogClosedByUser:
+                    console.log("Dialog was closed by user");
+                    break;
+                default:
+                    console.error(error.message);
+            }
+        });
+    }
 })();
